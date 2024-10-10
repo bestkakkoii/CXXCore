160305f321a45dff9f3041188aa5184818bc9968
bestkakkoii <bestkakkoii@gmail.com>
2024-10-10 16:56:31 +0800

    更新內存池實現並改用 GPL 版權聲明
    將 `cxxmemorypool.h` 和 `cxxmemorypool.cpp` 的版權聲明從 Box2D 版權聲明更改為 GNU General Public License (GPL)。
    
    將 `cxxmemorypool.h` 中的 `#include <cxxglobal.h>` 和 `#include <cstdint>` 替換為 `#include <memory_resource>`。
    
    移除了原本的內存池實現，並改用 C++17 的 `std::pmr::synchronized_pool_resource` 來實現內存池，並將內存池的默認大小設置為 1MB (`MemoryPoolDefaultPoolSize`)。
    
    更新了內存池的優點和缺點說明，並添加了使用方法的註釋。
    
    將 `CXXMemoryPool` 類模板化，並修改了內存分配和釋放的方法簽名，使其支持分配和釋放指定數量的對象。
    
    更新了 `CXXScopedMemoryPool` 類，使其與新的 `CXXMemoryPool` 類兼容。
    
    在 `CXXCoreTest.vcxproj.filters` 中，將 `cxxmemorypool` 的過濾器從 `old` 文件夾移動到 `template` 文件夾。

--- CXXCore-master/include/cxxmemorypool.h
+++ CXXCore-master/include/cxxmemorypool.h
@@ -1,101 +1,70 @@
 ﻿/*
-* Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
-*
-* This software is provided 'as-is', without any express or implied
-* warranty.  In no event will the authors be held liable for any damages
-* arising from the use of this software.
-* Permission is granted to anyone to use this software for any purpose,
-* including commercial applications, and to alter it and redistribute it
-* freely, subject to the following restrictions:
-* 1. The origin of this software must not be misrepresented; you must not
-* claim that you wrote the original software. If you use this software
-* in a product, an acknowledgment in the product documentation would be
-* appreciated but is not required.
-* 2. Altered source versions must be plainly marked as such, and must not be
-* misrepresented as being the original software.
-* 3. This notice may not be removed or altered from any source distribution.
+				GNU GENERAL PUBLIC LICENSE
+				   Version 2, June 1991
+COPYRIGHT (C) Bestkakkoii 2024 All Rights Reserved.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 */
-
-//bestkakkoii modified on 2022/11/08
-
 #ifndef CXXMEMORYPOOL_H_
 //@隐藏{
 #define CXXMEMORYPOOL_H_
 //@隐藏}
 
-
-
 #include <cxxglobal.h>
-
 #include <cstdint>
+#include <memory_resource>
 #include <mutex>
 #include <assert.h>
-
-/// This is a small object allocator used for allocating small
-/// objects that persist for more than one time step.
-/// See: http://www.codeproject.com/useritems/Small_Block_Allocator.asp
 
 //@别名 炫
 namespace cxx
 {
-	//@分组{［内存安全］.炫　內存池类
-
 	//@隐藏{
-#if _WIN64
-#pragma pack(push, 8)
-	constexpr size_t kChunkSize = 16i64 * 1024i64;    // default: 16kb (16 * 1024)
-	constexpr size_t kMaxBlockSize = 640i64;          // default: 640
-	constexpr size_t kBlockSizes = 60i64;             // default: 14
-	constexpr size_t kChunkArrayIncrement = 128i64;   // default: 128
-#pragma pack(pop)
-#else
-#pragma pack(push, 4)
-	constexpr size_t kChunkSize = 16 * 1024;     // default: 16kb (16 * 1024)
-	constexpr size_t kMaxBlockSize = 61440;      // default: 640
-	constexpr size_t kBlockSizes = 60;           // default: 14
-	constexpr size_t kChunkArrayIncrement = 128; // default: 128
-#pragma pack(pop)
-#endif
+	constexpr __int64 MemoryPoolDefaultPoolSize = 1024 * 1024; // 1MB default pool size
 	//@隐藏}
-
-	//@分组}
 }
 
 //@分组{［内存安全］.炫　內存池类
 
-//@隐藏{
-#if _WIN64
-#pragma pack(push, 8)
-#else
-#pragma pack(push, 4)
-#endif
-//@隐藏}
-
 //@备注 内存池的
-//优点 1.减少内存碎片 2.提高内存分配效率 3.提高内存分配效率
-//缺点 1.内存池的大小是固定的
+//优点 1.减少内存碎片 2.提高内存分配效率 3.提高内存分配效率 4.类型安全
+//缺点 1.初始内存池的大小是固定的，但可以通过参数调整，且可以根据需要增长
+// 使用场景 1.频繁申请和释放内存 2.需要高效的内存分配和释放 3.需要类型安全的内存分配和释放
+// 使用方法 1.创建内存池对象 2.申请内存 3.使用内存 4.释放内存 5.清空内存池
+// CXXMemoryPool memoryPool(1024); // 创建内存池对象
+// int* p = memoryPool.alloc(100); // 申请内存
+// memoryPool.free(p, 100); // 释放内存
+// memoryPool.clear(); // 清空内存池
 //@别名 炫内存池
+template<typename T>
 class CXXMemoryPool
 {
-	//@隐藏{
-	CXX_DISABLE_COPY_MOVE(CXXMemoryPool);
-	//@隐藏}
 public:
+	CXXMemoryPool(__int64 poolSize = cxx::MemoryPoolDefaultPoolSize);
 	virtual ~CXXMemoryPool();
 
-public:
 	//@备注 申请内存
-	//@参数 size 申请内存大小(64位系统下size_t为__int64, 32位系统下size_t为int) 
+	//@参数 count 申请对象的数量
 	//@返回 申请到的内存指针
-	//@别名 申请(大小)
-	void* alloc(size_t size);
+	//@别名 申请(数量)
+	T* alloc(__int64 count = 1);
 
 	//@备注 释放内存
 	//@参数 p 申请到的内存指针
-	//@参数 size 申请内存大小(64位系统下size_t为__int64, 32位系统下size_t为int)
+	//@参数 count 申请对象的数量
 	//@返回 无
-	//@别名 释放(指针, 大小)
-	void free(void* p, size_t size);
+	//@别名 释放(指针, 数量)
+	void free(T* p, __int64 count = 1);
 
 	//@备注 清空内存池
 	//@参数 无
@@ -103,37 +72,13 @@ public:
 	//@别名 清空()
 	void clear();
 
-	//@备注 设置内存池是否加锁
-	//@参数 enabled 是否加锁 TRUE:加锁 FALSE:不加锁 默認不加锁
-	//@返回 无
-	//@别名 置锁(是否加锁)
-	inline void setLockEnabled(bool enabled) { mutex_.lock(); lock_enabled_ = enabled; mutex_.unlock(); }
-
 private:
 	//@隐藏{
-	CXXMemoryPool();
+	CXX_DISABLE_COPY_MOVE(CXXMemoryPool);
 
-	typedef struct tagBlock
-	{
-		tagBlock* next = CXX_NULLPTR;
-	}Block;
-
-	typedef struct tagChunk
-	{
-		size_t block_size = 0;
-		Block* blocks = CXX_NULLPTR;
-	}Chunk;
-
-	size_t            num_chunk_count_ = 0;
-	size_t            num_chunk_space_ = 0;
-	Chunk* chunks_ = CXX_NULLPTR;
-	Block* free_lists_[cxx::kBlockSizes] = { CXX_NULLPTR };
-
-	BYTE			    s_block_size_lookup_[cxx::kMaxBlockSize + 1] = {};
-	bool              s_block_size_lookup_initialized_ = false;
-	bool              lock_enabled_ = false;
-	std::mutex          mutex_;
-	//@隐藏
+	std::unique_ptr<std::pmr::synchronized_pool_resource> pool_resource_;
+	std::pmr::memory_resource* upstream_resource_;
+	//@隐藏}
 };
 
 //@备注 使用 CXXScopedMemoryPool 内存池申请指定大小內存并自动在离开作用域后释放
@@ -144,59 +89,100 @@ private:
 template <typename T>
 class CXXScopedMemoryPool
 {
-
 public:
-	CXXScopedMemoryPool(size_t size)
-		: p_(CXXScopedMemoryPool::getInstance().alloc(size)), size_(size)
+	CXXScopedMemoryPool(__int64 count)
+		: p_(CXXMemoryPool<T>::getInstance().alloc(count)), count_(count)
 	{
-		assert(p_ != CXX_NULLPTR);
+		assert(p_ != nullptr);
 	}
-	virtual ~CXXScopedMemoryPool() { CXXScopedMemoryPool::getInstance().free(p_, size_); }
+	virtual ~CXXScopedMemoryPool() { CXXMemoryPool<T>::getInstance().free(p_, count_); }
 
 	//@备注 获取已申请到的内存
 	//@参数 无
 	//@返回 指定类型的内存指针
 	//@别名 取()
-	inline T* get() { return reinterpret_cast<T*>(p_); }
+	inline T* get() { return p_; }
 
 	//@备注 获取已申请到的内存
 	//@参数 无
 	//@返回 指定类型的内存指针
 	//@别名 地址()
-	inline T* data() { return reinterpret_cast<T*>(p_); }
+	inline T* data() { return p_; }
 
 	//@别名 到指针()
-	inline void* toVoidStar() { return p_; }
+	inline void* toVoidStar() { return static_cast<void*>(p_); }
 
 	//@别名 大小()
-	inline __int64 size() { return static_cast<__int64>(size_); }
+	inline int64_t size() { return static_cast<int64_t>(count_); }
 
-	inline __int64 count() { return size(); }
+	inline int64_t count() { return size(); }
 
-	inline __int64 length() { return size(); }
+	inline int64_t length() { return size(); }
 
 	//@别名 到整型()
 	inline int toInt() { return reinterpret_cast<int>(p_); }
 
 	//@别名 到正整型()
-	inline UINT toUInt() { return reinterpret_cast<unsigned int>(p_); }
+	inline unsigned int toUInt() { return reinterpret_cast<unsigned int>(p_); }
 
 	//@别名 到长整型()
-	inline __int64 toInt64() { return reinterpret_cast<__int64>(p_); }
+	inline int64_t toInt64() { return reinterpret_cast<int64_t>(p_); }
 
 	//@别名 到正长整型()
-	inline UINT64 toUInt64() { return reinterpret_cast<unsigned __int64>(p_); }
+	inline uint64_t toUInt64() { return reinterpret_cast<uint64_t>(p_); }
 
 private:
-	//@隐藏{
-	void* p_ = CXX_NULLPTR;
-	size_t size_ = 0;
-	//@隐藏 
+	T* p_ = nullptr;
+	__int64 count_ = 0;
 };
-//@分组}
 
 //@隐藏{
-#pragma pack(pop)
+template<typename T>
+CXXMemoryPool<T>::CXXMemoryPool(__int64 poolSize)
+{
+	upstream_resource_ = std::pmr::new_delete_resource();
+	pool_resource_ = std::make_unique<std::pmr::synchronized_pool_resource>(
+		std::pmr::pool_options{ 0, std::numeric_limits<size_t>::max() },
+		upstream_resource_
+	);
+}
+
+template<typename T>
+CXXMemoryPool<T>::~CXXMemoryPool() = default;
+
+template<typename T>
+T* CXXMemoryPool<T>::alloc(__int64 count)
+{
+	if (count <= 0)
+	{
+		return nullptr;
+	}
+
+	return static_cast<T*>(pool_resource_->allocate(static_cast<size_t>(count) * sizeof(T), alignof(T)));
+}
+
+template<typename T>
+void CXXMemoryPool<T>::free(T* p, __int64 count)
+{
+	if (p == nullptr || count <= 0)
+	{
+		return;
+	}
+
+	pool_resource_->deallocate(p, static_cast<size_t>(count) * sizeof(T), alignof(T));
+}
+
+template<typename T>
+void CXXMemoryPool<T>::clear()
+{
+	// Recreate the pool to achieve a similar effect to clearing.
+	pool_resource_ = std::make_unique<std::pmr::synchronized_pool_resource>(
+		std::pmr::pool_options{ 0, std::numeric_limits<size_t>::max() },
+		upstream_resource_
+	);
+}
 //@隐藏}
+
+//@分组}
 
 #endif // CXXMEMORYPOOL_H_
